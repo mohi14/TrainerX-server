20ff75d7c7c8ebcf26dc387e28662ccc40698768
mohi14 <mohiuddinkhan1409@gmail.com>
2022-11-09 19:08:14 +0600

    routes for a specific service added

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 5000
 require('dotenv').config()
 
@@ -33,6 +33,13 @@ async function run() {
             const cursor = servicesCollection.find(query);
             const services = await cursor.toArray();
             res.send(services);
+        })
+
+        app.get('/services/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _Id: ObjectId(id) };
+            const service = await servicesCollection.findOne(query);
+            res.send(service);
         })
     }
     finally {
