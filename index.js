e2c67df7c5ff3b42400e91db5245c5760a2ee86f
mohi14 <mohiuddinkhan1409@gmail.com>
2022-11-09 13:05:15 +0600

    server setup done

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// server setup
+const express = require('express')
+const app =express()
+const cors = require('cors')
+const port = process.env.PORT || 5000
+require('dotenv').config()
+
+app.use(cors())
+app.use(express.json())
+
+app.get('/',(req,res)=>{
+    res.send('server running')
+})
+
+app.listen(port,()=>{
+    console.log(`TrainerX server running on ${port}`)
+})
