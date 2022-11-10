323dddd3c477fb534aad52a6a41227e70a6fddb0
mohi14 <mohiuddinkhan1409@gmail.com>
2022-11-10 18:56:13 +0600

    vercel deploy done

--- index.js
+++ index.js
@@ -17,17 +17,28 @@ app.get('/', (req, res) => {
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.3dkasq3.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+function verifyJWT(req, res, next) {
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+        return res.status(401).send({ message: 'unauthorized access' })
+    }
+    const token = authHeader.split(' ')[1];
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function (err, decoded) {
+        if (err) {
+            return res.status(403).send({ message: 'unauthorized access' })
+        }
+        req.decoded = decoded;
+        next();
+    })
+
+}
+
 async function run() {
     try {
         const servicesCollection = client.db('trainerX').collection('services')
         const reviewsCollection = client.db('trainerX').collection('reviews')
 
-        app.post('/jwt', (req, res) => {
-            const user = req.body;
-            const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1hr' })
-            console.log(user)
-            res.send({ token })
-        })
+
 
         app.get('/servicesHome', async (req, res) => {
             const query = {};
@@ -41,6 +52,13 @@ async function run() {
             const cursor = servicesCollection.find(query).sort({ _id: -1 });
             const services = await cursor.toArray();
             res.send(services);
+        })
+
+        app.post('/jwt', (req, res) => {
+            const user = req.body;
+            const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1hr' })
+            // console.log(user)
+            res.send({ token })
         })
 
         app.get('/services/:id', async (req, res) => {
@@ -67,6 +85,11 @@ async function run() {
         })
 
         app.get('/reviews', async (req, res) => {
+            // const decoded = req.decoded;verifyJWT,
+            // if (decoded.email !== req.query.email) {
+            //     res.status(403).send({ message: 'Forbidden access' })
+            // }
+
             let query = {}
             if (req.query.email) {
                 query = {
